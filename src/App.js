a2226ad3d543b86fd8f298b860be5a9fb2c7c9ea
hermesalexis1 <hermesagm@gmail.com>
2018-07-05 00:03:37 -0500

    resolve challenge 1

--- src/App.js
+++ src/App.js
@@ -5,14 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Mi primera aplicación</h1>
+        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
       </div>
     );
   }
